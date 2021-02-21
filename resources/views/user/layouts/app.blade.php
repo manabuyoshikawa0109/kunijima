46972bc73606f6354c75c767c01d5a0968bf51d8
吉川学 <m.y.ichinishi.tennis@gmail.com>
2021-02-21 23:52:55 +0900

    認証系のビューファイルを一旦userディレクトリへ移動 ユーザーログイン後画面のベースとなるファイルを追加 welcome.blade.phpファイルの削除

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>くにじまテニスコート || ご利用案内</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
