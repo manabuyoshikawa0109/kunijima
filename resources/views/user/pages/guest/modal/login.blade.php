382cd95082f732608e25c383357676f8fbc938d3
吉川学 <m.y.ichinishi.tennis@gmail.com>
2022-02-06 01:29:49 +0900

    cssが読み込まれないのでtype属性を削除して検証

--- resources/views/user/pages/guest/modal/login.blade.php
+++ resources/views/user/pages/guest/modal/login.blade.php
@@ -2,9 +2,9 @@
 <!-- Google Font -->
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 <!-- CSS -->
-<link rel="stylesheet" type="text/css" href="/common/plugins/deskapp/vendors/styles/core.css">
-<link rel="stylesheet" type="text/css" href="/common/plugins/deskapp/vendors/styles/icon-font.min.css">
-<link rel="stylesheet" type="text/css" href="/common/plugins/deskapp/vendors/styles/style.css">
+<link rel="stylesheet" href="/common/plugins/deskapp/vendors/styles/core.css">
+<link rel="stylesheet" href="/common/plugins/deskapp/vendors/styles/icon-font.min.css">
+<link rel="stylesheet" href="/common/plugins/deskapp/vendors/styles/style.css">
 @endpush
 
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
